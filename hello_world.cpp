6d946a693b84a8798301bda1c399ee3a2c1598a8
Amirullah Miswan <amirullahmiswan@amirullah-macbook.dyndns.cern.ch>
2016-06-21 10:41:24 +0200

    hi there.. it seems that you have make some tidious work on std. it's better if u just use the global declaration. i have edited it. check it out.
    regards

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,12 @@
 //my first program to be used in github
 
 #include<iostream>
+using namespace std;
 
 int main()
 {
-std:cout<< “Hello World”;
+cout<< “Hello World\n”;
+cout<<“What is your name?”;
+cin>>name;
+out<<“Welcome ”<<name<<“ to your first github process\n”;
 }
